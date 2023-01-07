8554001a58c9d3e0057ce8d0bdb90f5cb474d6df
Cauan Victor <91569130+CauanDZN@users.noreply.github.com>
2023-01-07 17:36:28 -0300

    feat: Component Display

--- App.js
+++ App.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 import Button from './src/components/Button';
+import Display from './src/components/Display';
 
 export default class App extends Component {
+
+  state = {
+    displayValue: '0',
+  }
+
   render(){
     return(
       <SafeAreaView>
+        <Display value={this.state.displayValue} />
         <View style={styles.buttons}>
           <Button label='AC' />
           <Button label='/' />
